10b8d5c10ad8a06798db772e233703a95dd8c996
Bram Mortier <79015250+BramMortier@users.noreply.github.com>
2024-12-19 23:49:58 +0100

    form functionality added

---
+++ src/cms/services/index.js
@@ -0,0 +1,1 @@
+export const services = ["Kleuradvies", "Vloeren", "3D Ontwerpen", "Meubels"]
--- src/scripts/components/Form.js
+++ src/scripts/components/Form.js
@@ -4,14 +4,72 @@ class Form extends HTMLElement {
 	}
 
 	connectedCallback() {
-		this.setAttribute("role", "form")
-
+		this.initElements()
 		this.initEvents()
-
-		console.log("form component")
 	}
 
-	initEvents() {}
+	initElements() {
+		this.serviceButtons = [...this.querySelectorAll(".form__service")]
+		this.questionsContainer = this.querySelector(".form__questions")
+	}
+
+	initEvents() {
+		this.serviceButtons.forEach((button) => {
+			button.addEventListener("click", () => {
+				this.addQuestionBlock(button.dataset.service)
+			})
+		})
+	}
+
+	addQuestionBlock(service) {
+		const element = document.createElement("div")
+		element.classList.add("form-question")
+
+		element.innerHTML = `
+			<ul class="form__question">
+				<h3 class="c-title">${service}</h3>
+
+				<div class="c-input">
+					<label class="c-input__label typo-body-medium" for="room">Ruimte</label>
+
+					<input
+						type="text"
+						id="room"
+						name="room"
+						placeholder="Badkamer, Living, ..."
+						class="c-input__field typo-body-medium"
+					/>
+				</div>
+				
+				<div class="c-input">
+					<label class="c-input__label typo-body-medium" for="surface-area">Oppervlakte</label>
+
+					<input
+						type="text"
+						id="surface-area"
+						name="surface-area"
+						placeholder="m²"
+						class="c-input__field typo-body-medium"
+					/>
+				</div>
+
+				<div class="c-textarea">
+					<label class="c-textarea__label typo-body-medium" for="info"
+						>Extra opmerkingen</label
+					>
+					<textarea
+						id="info"
+						name="info"
+						placeholder="Opmerkingen"
+						class="c-textarea__field typo-body-medium"
+					>
+					</textarea>
+				</div>
+			</ul>
+		`
+
+		this.questionsContainer.appendChild(element)
+	}
 }
 
 customElements.define("js-form", Form)
